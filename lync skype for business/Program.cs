d6322fd855ebc77062411d1eda4a52d4e9160082
tonyfickas <tony@tonysbox.com>
2015-08-04 07:46:01 -0700

    initial check-in
    initial check-in 100% working

---
+++ lync skype for business/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Diagnostics;
+
+namespace lync_skype_for_business
+{
+    class Program
+    {
+        //define settings and lync data location
+        public const string regBase = @"Software\Microsoft\Office";
+        public const string subKey = "Lync";
+        private const string lyncPath = @"C:\Program Files\Microsoft Office\Office15\lync.exe";
+        private const string arguments = "/fromrunkey";
+        
+        static void Main(string[] args)
+        {
+            //on  = 00, 00, 00, 01
+            //off = 00, 00, 00, 00
+            registry.Write("EnableSkypeUI", new byte[] { 00, 00, 00, 01});
+
+            Process cmd = new Process();
+
+            //define lync to be launched
+            cmd.StartInfo.FileName = lyncPath;
+
+            //define arguments for lync.exe
+            cmd.StartInfo.Arguments = arguments;
+
+            //launch process
+            cmd.Start();
+
+        }
+    }
+}
---
+++ lync skype for business/Registry.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Win32;
+using System;
+
+namespace lync_skype_for_business
+{
+    public class registry
+    {
+        #region Write
+        public static bool Write(string KeyName, byte[] Value, string openSubKey = Program.subKey)
+        {
+            try
+            {
+                RegistryKey rk = Registry.CurrentUser.OpenSubKey(Program.regBase, true);
+                RegistryKey sk1 = rk.CreateSubKey(openSubKey);
+                // Save the value
+                sk1.SetValue(KeyName, Value, RegistryValueKind.Binary);
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+        #endregion
+    }
+}

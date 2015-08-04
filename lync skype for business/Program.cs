6b1c63e2038847fff46c1d4af60d1c81d42a3899
tonyfickas <tony@tonysbox.com>
2015-08-04 08:29:20 -0700

    fixed 32bit bug
    Forgot not everyone runs 64bit, fixed issue where the 64bit path wasn't
    found because 32bit lync was installed

--- lync skype for business/Program.cs
+++ lync skype for business/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System.Diagnostics;
+using System.IO;
 
 namespace lync_skype_for_business
 {
@@ -7,25 +8,41 @@ namespace lync_skype_for_business
         //define settings and lync data location
         public const string regBase = @"Software\Microsoft\Office";
         public const string subKey = "Lync";
-        private const string lyncPath = @"C:\Program Files\Microsoft Office\Office15\lync.exe";
+        private const string lyncPath32 = @"C:\Program Files (x86)\Microsoft Office\Office15\lync.exe";
+        private const string lyncPath64 = @"C:\Program Files\Microsoft Office\Office15\lync.exe";
         private const string arguments = "/fromrunkey";
         
         static void Main(string[] args)
         {
-            //on  = 00, 00, 00, 01
-            //off = 00, 00, 00, 00
-            registry.Write("EnableSkypeUI", new byte[] { 00, 00, 00, 01});
+            try
+            {
+                //on  = 00, 00, 00, 01
+                //off = 00, 00, 00, 00
+                registry.Write("EnableSkypeUI", new byte[] { 00, 00, 00, 01});
 
-            Process cmd = new Process();
+                Process cmd = new Process();
 
-            //define lync to be launched
-            cmd.StartInfo.FileName = lyncPath;
+                if (File.Exists(lyncPath32))
+                {
+                    //define lync to be launched 32bit
+                    cmd.StartInfo.FileName = lyncPath32;
+                }
+                else
+                {
+                    //define lync to be launched 64bit
+                    cmd.StartInfo.FileName = lyncPath64;
+                }
 
-            //define arguments for lync.exe
-            cmd.StartInfo.Arguments = arguments;
+                //define arguments for lync.exe
+                cmd.StartInfo.Arguments = arguments;
 
-            //launch process
-            cmd.Start();
+                //launch process
+                cmd.Start();
+
+            }
+            catch (System.Exception)
+            {
+            }
 
         }
     }
